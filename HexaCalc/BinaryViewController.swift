19d6fcd13d22a259fdff345a1c87d9c00a3d8bca
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2020-07-20 00:43:20 -0400

    Setting up the three calculator tabs

--- HexaCalc/BinaryViewController.swift
+++ HexaCalc/BinaryViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class BinaryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- HexaCalc/HexadecimalViewController.swift
+++ HexaCalc/HexadecimalViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class HexadecimalViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
