5c3b6e44ee8a500a0c1d9bc94a2342a5f6cadb98
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2020-07-24 16:53:37 -0400

    Adding binary calculator completed UI

--- HexaCalc/BinaryViewController.swift
+++ HexaCalc/BinaryViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class BinaryViewController: UIViewController {
-
+    
     var stateController: StateController?
+    
+    let binaryDefaultLabel:String = "0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000"
     
     @IBOutlet weak var outputLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        outputLabel.text = binaryDefaultLabel
+    }
+    
+    //Load the current converted value from either of the other calculator screens
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        var newLabelValue = stateController?.convValues.binVal
+        if (newLabelValue == "0") {
+            newLabelValue = binaryDefaultLabel
+        }
+        //Need to format for binary representation
+        else {
+            
+        }
+        outputLabel.text = newLabelValue
     }
 
 
+    //MARK: Private Functions
 }
 
 //Adds state controller to the view controller
