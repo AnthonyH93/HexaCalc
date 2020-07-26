6eafe2e941a6d2f75ff08a89e48ea607c11f2f37
AnthonyH93 <ajpnhopk@uwaterloo.ca>
2020-07-25 23:52:25 -0400

    Button actions added for binary calculator

--- HexaCalc/BinaryViewController.swift
+++ HexaCalc/BinaryViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class BinaryViewController: UIViewController {
     
+    //MARK: Properties
     var stateController: StateController?
     
     let binaryDefaultLabel:String = "0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000"
@@ -38,9 +39,76 @@ class BinaryViewController: UIViewController {
         outputLabel.text = newLabelValue
     }
 
-
-    //MARK: Private Functions
+    //MARK: Button Actions
+    @IBAction func numberPressed(_ sender: RoundButton) {
+        
+    }
     
+    @IBAction func ACPressed(_ sender: RoundButton) {
+        
+    }
+    
+    @IBAction func plusMinusPressed(_ sender: RoundButton) {
+        
+    }
+    
+    @IBAction func deletePressed(_ sender: RoundButton) {
+        
+    }
+    
+    @IBAction func leftShiftPressed(_ sender: RoundButton) {
+        
+    }
+    
+    @IBAction func rightShiftPressed(_ sender: RoundButton) {
+        
+    }
+    
+    @IBAction func onesCompPressed(_ sender: RoundButton) {
+        
+    }
+    
+    @IBAction func twosCompPressed(_ sender: RoundButton) {
+        
+    }
+    
+    @IBAction func XORPressed(_ sender: RoundButton) {
+        
+    }
+    
+    @IBAction func ORPressed(_ sender: RoundButton) {
+        
+    }
+    
+    @IBAction func ANDPressed(_ sender: RoundButton) {
+        
+    }
+    
+    @IBAction func signedModeClicked(_ sender: RoundButton) {
+        
+    }
+    
+    @IBAction func dividePressed(_ sender: RoundButton) {
+        
+    }
+    
+    @IBAction func multiplyPressed(_ sender: RoundButton) {
+        
+    }
+    
+    @IBAction func minusPressed(_ sender: RoundButton) {
+        
+    }
+    
+    @IBAction func addPressed(_ sender: RoundButton) {
+        
+    }
+    
+    @IBAction func equalsPressed(_ sender: RoundButton) {
+        
+    }
+    
+    //MARK: Private Functions
     func formatBinaryString(stringToConvert: String) -> String {
         var manipulatedStringToConvert = stringToConvert
         while (manipulatedStringToConvert.count < 64){
